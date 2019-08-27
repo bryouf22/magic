b1a2090c159cd4adfba6c2be7f6ad1bad350fc2c
annro <ronan.bernard@gmail.com>
2019-08-27 22:34:15 +0200

    correctif

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,8 @@ set :repo_url, "https://github.com/annro-git/magic.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', "config/master.key"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_files, 'config/secrets.yml'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
