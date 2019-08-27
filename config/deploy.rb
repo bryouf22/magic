071cf23069c1c3e43d773a0482b6255958631db6
Jean Valjean <bryouf@free.fr>
2019-08-27 22:24:04 +0200

    mastey key in shared config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,19 +7,7 @@ set :repo_url, "https://github.com/annro-git/magic.git"
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
-
-namespace :deploy do
-  namespace :check do
-    before :linked_files, :set_master_key do
-      on roles(:app), in: :sequence, wait: 10 do
-        unless test("[ -f #{shared_path}/config/master.key ]")
-          upload! 'config/master.key', "#{shared_path}/config/master.key"
-        end
-      end
-    end
-  end
-end
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', "config/master.key"
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 5
