c1e7e8dbb7af6d1374c79ef2aa47643ada601f8c
Jean Valjean <bryouf@free.fr>
2020-05-23 00:19:24 +0200

    fix percent calculation

--- app/interactors/deck/calculate_percent_complete.rb
+++ app/interactors/deck/calculate_percent_complete.rb
@@ -4,6 +4,6 @@ class Deck::CalculatePercentComplete
   def call
     deck = Deck.find(context.deck_id)
 
-    context.complete_percent = 100 - (deck.missing_cards.sum { |_k, v| v }.to_f / deck.card_number * 100).round.to_i
+    context.complete_percent = 100 - (deck.missing_cards.sum { |_k, v| v }.to_f / deck.card_number * 100)&.round&.to_i || 0
   end
 end
