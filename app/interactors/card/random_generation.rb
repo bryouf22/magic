cecb98e5c1745173544b88319bbd336dfac3a3d0
Jean Valjean <bryouf@free.fr>
2020-07-26 15:25:10 +0200

    css

--- app/interactors/card/random_generation.rb
+++ app/interactors/card/random_generation.rb
@@ -2,23 +2,9 @@ class Card::RandomGeneration
   include Interactor
 
   def call
-    0/0
   end
 
   def color
-    w 17
-    g 17
-    r 17
-    u 17
-    b 17
-    n 15
   end
 
-  def type
-    :creature
-    :land
-    :enchantment
-    :sorcery
-    :instant
-    :
 end
