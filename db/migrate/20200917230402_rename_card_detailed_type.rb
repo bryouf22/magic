b9a11cdc81f738d3788f5b75c0f074900e2fdf27
Jean Valjean <bryouf@free.fr>
2020-09-18 01:15:48 +0200

    fix types

--- db/migrate/20200917230402_rename_card_detailed_type.rb
+++
@@ -1,6 +0,0 @@
-class RenameCardDetailedType < ActiveRecord::Migration[5.2]
-  def change
-    Card.find(11664).&update(detailed_type: 'Creature — Elemental')
-    Card.other.map(&:set_type!)
-  end
-end
